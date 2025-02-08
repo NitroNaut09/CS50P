5b16b3f6f757637be6c67947517d5ec6e7d2e6ec
NitroNaut09 <hrishikeshdeeraj@gmail.com>
2025-02-08 23:09:20 +0530

    First commit on Debian Bookworm! yay

---
+++ list.py
@@ -0,0 +1,7 @@
+for i in range(3):
+	print('meow')
+
+students = ['slytherin', 'gryffindor', 'hufflepuff', 'ravenclaw']
+houses = ['draco', 'harry', 'cedric', 'luna']
+for i in range(len(students)):
+	print(f'{students[i]}: {houses[i]}')
